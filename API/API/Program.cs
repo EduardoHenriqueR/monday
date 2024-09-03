477eaa5c4bb9e1bf4e2d7d47786b37b96e9a5e8b
Pedromed10 <pedromed.silva05@gmail.com>
2024-09-02 21:44:10 -0300

    Teste.http e modificações no program

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,6 +1,14 @@
+//Minimal APIs em c#
+//Testar a API
+// - Rest Client - Extensão Vs
+// - Postman
+// - Insomnia
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Endpoint - FUncionalidades
+//Requisição - URL e método/verbo HTTP
+app.MapGet("/", () => "Genshin could never.");
+app.MapGet("/b", () => "0w0.");
 
 app.Run();
