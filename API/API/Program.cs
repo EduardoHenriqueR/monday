512b7e3d14af9eb7775ad4c742679e21f4fdae74
Eduardo <ehra3211@gmail.com>
2024-09-08 19:31:50 -0300

    Atividades 10/09

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,3 +1,4 @@
+using API;
 //Minimal APIs em c#
 //Testar a API
 // - Rest Client - Extensão Vs
@@ -10,5 +11,16 @@ var app = builder.Build();
 //Requisição - URL e método/verbo HTTP
 app.MapGet("/", () => "Genshin could never.");
 app.MapGet("/b", () => "0w0.");
+app.MapGet("/repositorio/{usuario}/{repositorio}", (string usuario, string repositorio) =>
+{
+    return $"Usuário: {usuario}, Repositório: {repositorio}";
+});
+
+app.MapPost("/usuario", (Usuario usuario) => 
+{
+    return Results.Ok($"Usuário {usuario.Nome}, {usuario.Idade} anos, recebido com sucesso.");
+});
+
+
 
 app.Run();
---
+++ API/API/Usuario.cs
@@ -0,0 +1,8 @@
+namespace API;
+
+public class Usuario
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+
+}
